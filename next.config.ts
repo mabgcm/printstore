7e728aa790bca1e59a3af058b687edf43cc6acb6
mabgcm <bugucam@gmail.com>
2026-08-12 00:15:22 -0400

    fix: allow Printify mockup images

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.printify.com",
       },
+      {
+        protocol: "https",
+        hostname: "images-api.printify.com",
+      },
     ],
   },
   turbopack: {
